9046b4cf14f954bfb7c5bb81d133a77268c8b9c2
pedrozelada <przs.gideon@gmail.com>
2024-11-16 11:15:03 -0400

    week2

--- controllers/users.js
+++ controllers/users.js
@@ -19,7 +19,63 @@ const getSingle = async (req, res) => {
  });
 };
 
+const createUser = async (req, res) => {
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('contacts').insertOne(user);
+    if (response.acknowledged) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error ocurred while creating new user');
+    }
+};
+
+const updateUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    try {
+        const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({ _id: new ObjectId(userId) }, user);
+
+        if (response.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(404).json({ error: 'User not found or no changes made' });
+        }
+    } catch (error) {
+        console.error('Error updating user:', error);
+        res.status(500).json({ error: error.message || 'Some error occurred while updating the user' });
+    }
+};
+
+const deleteUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    
+    const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({ _id: userId});
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error ocurred while updating the user');
+    }
+};
+
+
+
+
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
 }
--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.Port || 3000;
 
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 app.use('/', require ('./routes'));
+
+
 
 mongodb.initDb((err) => {
     if(err) {
